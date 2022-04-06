d7c7be5a07e224324c06238db76ed418f2c60548
OLMM-2022 <103146978+OLMM-2022@users.noreply.github.com>
2022-04-06 19:56:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-
+from cifar import loadcifar, OLDS
+from reuter import loadreuter, OLDS_reuter
 
 if __name__ == '__main__':
     print('trainning starts')
-    x_S1, x_S2, y_S1, y_S2 = loaddataset()
+    x_S1, x_S2, y_S1, y_S2 = loadcifar()
     train = OLDS(x_S1, y_S1, x_S2, y_S2, 50000, 5000)
     train.T_2()
 
